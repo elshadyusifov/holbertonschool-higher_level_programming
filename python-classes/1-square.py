7ce3b0d1083d94771f8bf9ff57a3e0a9fb744fc3
elshadyusifov <e.yusifov335@gmail.com>
2025-05-22 02:17:55 +0400

    class def size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+this module 1-square right now it is empty
+'''
+
+
+class Square:
+    '''
+    squre class is empty
+    '''
+    def __init__(self, size):
+        self.__size = size
