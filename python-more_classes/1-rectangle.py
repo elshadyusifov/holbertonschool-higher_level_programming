b74fdbe9b02f48a399d3b11a06cbbab513585901
elshadyusifov <e.yusifov335@gmail.com>
2025-05-25 16:54:09 +0400

    property getter setter for rectangle 2

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     this module 1-rectangle is Real definition of a rectangle
     '''
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
     
     @property
     def width(self):
