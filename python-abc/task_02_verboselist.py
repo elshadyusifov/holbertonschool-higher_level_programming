3af29f614d60797f9762ec8b7ede5c4abbd6ce6f
elshadyusifov <e.yusifov335@gmail.com>
2025-06-01 23:51:47 +0400

    bet

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,18 @@
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+    
+    def extend(self, iterable):
+        items_added = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{items_added}] items.")
+    
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+    
+    def pop(self, index=-1):
+        item = self[index]  # Get the item before popping
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
