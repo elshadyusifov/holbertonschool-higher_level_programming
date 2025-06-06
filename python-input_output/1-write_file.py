8dbe05b2fa129ea43d20b08d9d222a400b4382d5
elshadyusifov <e.yusifov335@gmail.com>
2025-06-07 00:30:45 +0400

    write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a function to write a
+UTF-8 text file."""
+
+def write_file(filename="", text=""):
+    """write a UTF-8 text file."""
+    with open(filename, "w", encoding=utf-8) as file:
+        file.write(text)
