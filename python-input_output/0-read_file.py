ac6ea443a1c65a47e10ba9340320483ec277dc52
elshadyusifov <e.yusifov335@gmail.com>
2025-06-06 23:46:59 +0400

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""This module contains a function to read a UTF-8 text file and print its content."""
+
 
 def read_file(filename=""):
     with open("my_file_0.txt", "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+        print(file.read())
