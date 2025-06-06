77bf6a40de4ef9aabc33706e3ac2e064b2fb08cd
elshadyusifov <e.yusifov335@gmail.com>
2025-06-06 23:57:39 +0400

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
-    with open("filename", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
