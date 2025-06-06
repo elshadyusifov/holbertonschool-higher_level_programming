fd6327179715bc6d950d6fbbd1be9f06ccde6b1a
elshadyusifov <e.yusifov335@gmail.com>
2025-06-06 20:54:46 +0400

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 def read_file(filename=""):
     pass
 
-with open("my_file_0.txt", "r", encodinf="utf-8") as myfile:
+with open("my_file_0.txt", "r", encoding="utf-8") as myfile:
     print(myfile.read())
