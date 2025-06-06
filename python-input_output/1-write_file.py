bddbcda65892d5b1cf75bd5a6cadea41270204cc
elshadyusifov <e.yusifov335@gmail.com>
2025-06-07 00:43:37 +0400

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 UTF-8 text file."""
 
 def write_file(filename="", text=""):
+
+    
     """write a UTF-8 text file."""
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
