18b04432510de7f85deae26252f85af569693281
elshadyusifov <e.yusifov335@gmail.com>
2025-06-07 18:52:40 +0400

    load json file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     '''load from json file'''
-    with open(filename, "r", encoding="uft-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         my_obj = json.load(file)
         return my_obj
