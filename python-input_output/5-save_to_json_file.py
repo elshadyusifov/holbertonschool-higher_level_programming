c6c29da7f2c90a78172cf107c414ac9592368ca1
elshadyusifov <e.yusifov335@gmail.com>
2025-06-07 18:02:05 +0400

    save json file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''save obj to a file'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dumps(my_obj, file, ensure_ascii=False, indent=4)
+        return filename
