c0c39d50ce14f699889014c9f3b16d769627fe6e
elshadyusifov <e.yusifov335@gmail.com>
2025-06-07 10:06:00 +0400

    append file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ UTF-8 text file."""
 def append_write(filename="", text=""):
     """append a UTF-8 text file."""
     with open(filename, "w", encoding="utf-8"):
-        return filename.append(text)
+        return filename.append(str(text))
