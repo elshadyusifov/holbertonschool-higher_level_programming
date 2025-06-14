b61da9c75691c4370b221cfe29fe41a3b79e7a1d
elshadyusifov <e.yusifov335@gmail.com>
2025-06-14 23:04:50 +0400

    pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-'''create pascal triangle'''
+"""
+Pascal's Triangle Module
+"""
 
 
 def pascal_triangle(n):
-    if isinstance(n, int):
-        if n <= 0:
-            return None
-    return n
+    """Calculate Pascal's Triangle up to n rows."""
+    triangle = []
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            prev_row = triangle[-1]
+            row = [1]  # start with 1
+            # sum of adjacent elements in the previous row
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)  # end with 1
+            triangle.append(row)
+    return triangle
