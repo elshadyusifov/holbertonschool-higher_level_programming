c64b5209db17b850d3964b8acaf4ae8ee31165b0
elshadyusifov <e.yusifov335@gmail.com>
2025-10-15 14:32:00 +0000

    force name table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- Create the table 'force_name' if it does not already exist
 CREATE TABLE IF NOT EXISTS force_name (
-  id INT PRIMARY KEY,         -- id column is the primary key
+  id INT,         -- id column is the primary key
   name VARCHAR(256) NOT NULL  -- name column cannot be NULL
 );
 
